dbb6c3a50ca8f23521800feb8532cba8afc37be7
Brwa Yaseen <brwayasin29@gmail.com>
2024-05-11 20:46:28 +0300

    new

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,11 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className=" h-full">
-       <body className={inter.className}>
+        <body className={cn(" relative h-full font-sans antialiased", inter.className)}>
          <main className=" relative flex flex-col min-h-screen">
           <Providers>
           <Navbar/>
-          <div className=" scale-95 flex-grow flex-1">
+          <div className=" scale-90 flex-grow flex-1">
             {children}
           </div>
           </Providers>
--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { ShoppingCart } from "lucide-react";
 import { Sheet, SheetTrigger, SheetContent, SheetTitle, SheetHeader, SheetFooter } from "./ui/sheet";
 import { Separator } from "./ui/separator";
@@ -5,28 +7,50 @@ import { formatPrice } from "@/lib/utils";
 import Link from "next/link";
 import Image from "next/image";
 import { buttonVariants } from "./ui/button";
+import { useCart } from "@/hooks/use-cart";
+import { useEffect, useState } from "react";
+import { ScrollArea } from "./ui/scroll-area";
+import CartItem from "./CartItem";
 
 const Cart = () => {
+  const { items } = useCart()
+  const itemCount = items.length
 
-    const itemCount = 0
-  
-    const Fee = 1
+  const [isMounted, setIsMounted] = useState<boolean>(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  const fee = 1
+
+  const cartTotal = items.reduce(
+    (total, { product }) => total + product.price,
+    0
+  )
     return (
       <Sheet>
           <SheetTrigger aria-hidden="true" className=" group -m-2 flex items-center p-2">
               <ShoppingCart className=" h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
               <span className=" ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-                  0
+              {isMounted ? itemCount : 0}
               </span>
               </SheetTrigger>
               <SheetContent className=" flex w-full flex-col pr-0 sm:max-w-lg">
                   <SheetHeader className=" space-y-2.5 pr-6">
-                     <SheetTitle>Cart (0)</SheetTitle> 
+                     <SheetTitle>Cart ({itemCount})</SheetTitle> 
                   </SheetHeader>
                   {itemCount > 0 ? (
                     <>
                     <div className=" flex w-full flex-col pr-6">
-                      Cart Items
+                    <ScrollArea>
+                {items.map(({ product }) => (
+                  <CartItem
+                    product={product}
+                    key={product.id}
+                  />
+                ))}
+              </ScrollArea>
                     </div>
                     <div className=" space-y-4 pr-6">
                       <Separator/>
@@ -41,13 +65,13 @@ const Cart = () => {
                           <span className=" flex-1">
                             Transaction Fee
                           </span>
-                          <span>{formatPrice(Fee)}</span>
+                          <span>{formatPrice(fee)}</span>
                         </div>
                         <div className=" flex">
                           <span className=" flex-1">
                             Total
                           </span>
-                          <span>{formatPrice(Fee)}</span>
+                          <span> {formatPrice(cartTotal + fee)}</span>
                         </div>
                       </div>
                       <SheetFooter>
