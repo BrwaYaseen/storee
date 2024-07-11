f7c97b3dc55a61f9a9d4c620a2f37ee0deebea51
Brwa Yaseen <brwayasin29@gmail.com>
2024-07-11 18:35:20 +0300

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,53 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "localhost",
-                pathname: "**",
-                port: "3000"
-			},
-		],
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        pathname: "**",
+        port: "3000",
+      },
+    ],
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Handle .node files
+    config.module.rules.push({
+      test: /\.node$/,
+      use: "node-loader",
+    });
+
+    // Handle require.extensions issue
+    config.module.rules.push({
+      test: /\.js$/,
+      include: /node_modules\/payload/,
+      use: [
+        {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [
+              "@babel/plugin-proposal-class-properties",
+              "@babel/plugin-transform-runtime",
+            ],
+          },
+        },
+      ],
+    });
+
+    // Ignore specific warnings
+    config.ignoreWarnings = [
+      { module: /node_modules\/payload/ },
+      { module: /node_modules\/express/ },
+    ];
+
+    // Handle sharp on the server-side
+    if (isServer) {
+      config.externals.push("sharp");
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
