a2439858a3b56a708ad5a72404b588b6e87f59bf
Brwa Yaseen <brwayasin29@gmail.com>
2024-07-14 15:46:12 +0300

    new

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -40,7 +40,7 @@ const Page = async ({ params }: PageProps) => {
   });
 
   // Assert that products is an array of Product
-  const [product] = products as Product[];
+  const [product] = products;
 
   if (!product) return notFound();
 
