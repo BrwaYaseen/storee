9ce515299a0e511692ed46cf02bc7dd1a54c8d75
Brwa Yaseen <brwayasin29@gmail.com>
2024-09-24 17:21:37 +0300

    cleaned fresh

--- next.config.mjs
+++ next.config.mjs
@@ -1,39 +1,4 @@
-// next.config.mjs
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["localhost", "storee-production.up.railway.app"],
-    unoptimized: process.env.NODE_ENV === "production",
-  },
-  experimental: {
-    webpackBuildWorker: true,
-  },
-  webpack: (config, { isServer }) => {
-    // Handle .node files
-    config.module.rules.push({
-      test: /\.node$/,
-      use: "node-loader",
-    });
-
-    // Ignore specific warnings
-    config.ignoreWarnings = [
-      {
-        message:
-          /Critical dependency: the request of a dependency is an expression/,
-      },
-    ];
-
-    // Handle sharp on the server-side
-    if (isServer) {
-      config.externals.push("sharp");
-    }
-
-    // Disable cache to address serialization issues
-    config.cache = false;
-
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
