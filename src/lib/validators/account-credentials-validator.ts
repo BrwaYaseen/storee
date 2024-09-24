db9f907031950219eaaeff2c857d0e660319396e
Brwa Yaseen <brwayasin29@gmail.com>
2024-09-24 16:23:29 +0300

    cleaning up

--- src/lib/validators/account-credentials-validator.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod"
-
- export const AuthCredentialValidator = z.object({
-    email: z.string().email(),
-    password: z.string().min(8, {message:"password must be 8 characters or more"})
-  })
-
-  export type TAuthCredentialValidator = z.infer<typeof AuthCredentialValidator>
